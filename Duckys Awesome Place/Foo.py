f934cb890879b269d2df8d2f41be99e83e2cc0d6
TeardropDucky <201205925@post.au.dk>
2015-12-14 00:07:59 +0100

    maybe works

---
+++ Duckys Awesome Place/Foo.py
@@ -0,0 +1,35 @@
+class Empty:
+    def __init__(self):
+        self.IsEmpty = True
+
+    def __str__(self):
+        return "[]"
+
+class Node:
+    def __init__(self, x, xs):
+        self.IsEmpty = False
+        self.Value = x
+        self.Tail = xs
+
+    def __str__(self):
+        return str(self.Value) + "->" + str(self.Tail)
+
+def range(start, end):
+    lijst = Empty()
+    while start <= end:
+        end -= 1
+        lijst = Node(end, lijst)
+    return lijst
+
+def foo(start,end):
+    list = Empty()
+    if start < end:
+        list = range(start+1,end)
+    else:
+        list = Node(end,list)
+    return list
+
+print("Teacher Wrong Code")
+print(range(1,4))
+print("Awesome Ducky right Code")
+print(foo(1,4))
